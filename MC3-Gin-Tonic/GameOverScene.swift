6ea79a4f4cf40405ac7f26c2e9c4fdf10dd2f64a
alexmasu <alexmasu99@gmail.com>
2022-02-22 11:22:26 +0100

    Add Localizable function

--- MC3-Gin-Tonic/GameOverScene.swift
+++ MC3-Gin-Tonic/GameOverScene.swift
@@ -12,40 +12,63 @@ class GameOverScene: SKScene {
         
         
         super.init(size: size)
-        // Build the start game button:
-        //        restartButton.texture = SKTexture(imageNamed: "button")
+        
+        /*
+         Restart/Next Level Button definition
+         */
         let restartButton = SKSpriteNode(imageNamed: "button")
+        
         restartButton.size = CGSize(width: 295, height: 76)
+        
         // Name the start node for touch detection:
         restartButton.name = "RestartBtn"
-        restartButton.position = CGPoint(x: 200, y: 300)
         
+        restartButton.position = CGPoint(x: 200, y: 300)
         
         addChild(restartButton)
         
-        let restartText = SKLabelNode(fontNamed:
-                                        "AdventPro-Bold")
+        /*
+         Restart/Next Level Button Text definition
+         */
+        let restartText = SKLabelNode(fontNamed:"AdventPro-Bold")
         
         let buttonMessage = won ? "Next Level" : "RESTART"
-        restartText.text = buttonMessage
+        
+        restartText.text = buttonMessage.localized()
+        
         restartText.verticalAlignmentMode = .center
+        
         restartText.position = .zero
+        
         restartText.fontSize = 40
+        
         restartText.fontColor = .black
+        
         // Name the text node for touch detection:
         restartText.name = "RestartBtn"
+        
         restartText.zPosition = 5
+        
         restartButton.addChild(restartText)
         
         backgroundColor = SKColor.blue
+        
+        /*
+         Won/Game Over Label definition
+         */
         let message = won ? "You Won!" : "Game Over"
         
         // 3
         let label = SKLabelNode(fontNamed: "AdventPro-Bold")
-        label.text = message
+        
+        label.text = message.localized()
+        
         label.fontSize = 60
+        
         label.fontColor = SKColor.red
+        
         label.position = CGPoint(x: 200, y: 500)
+        
         addChild(label)
     }
     

fd0cee567690db96ae089262563417d01b0b4b2e
MarieMittu <lady.marvis@icloud.com>
2022-02-23 11:50:15 +0100

    Pause screen

--- MC3-Gin-Tonic/PauseScreen.swift
+++ MC3-Gin-Tonic/PauseScreen.swift
@@ -17,12 +17,19 @@ class PauseScreen: SKSpriteNode {
 //            particles.zPosition = -1
 //            addChild(particles)
 //        }
-        super.init(texture: nil, color: .blue, size: CGSize(width: 300, height: 500))
+        super.init(texture: nil, color: .clear, size: CGSize(width: 270, height: 270))
+        self.zPosition = 200
+        let background = SKSpriteNode(imageNamed: "pausebg")
+        background.zPosition = 1
+        background.position = .zero
+        addChild(background)
+        
         let resumeButton = SKSpriteNode(imageNamed: "button")
-        resumeButton.size = CGSize(width: 295, height: 76)
+        resumeButton.size = CGSize(width: 150, height: 60)
         // Name the start node for touch detection:
         resumeButton.name = "ResumeBtn"
         resumeButton.position = .zero
+        resumeButton.zPosition = 5
         
         
         self.addChild(resumeButton)
@@ -30,39 +37,50 @@ class PauseScreen: SKSpriteNode {
         let resumeText = SKLabelNode(fontNamed:
                                         "AdventPro-Bold")
         
-        let buttonMessage = "Resume"
+        let buttonMessage = "CONTINUE"
         resumeText.text = buttonMessage
         resumeText.verticalAlignmentMode = .center
         resumeText.position = .zero
-        resumeText.fontSize = 40
+        resumeText.fontSize = 32
         resumeText.fontColor = UIColor(rgb: 0x001273)
         // Name the text node for touch detection:
         resumeText.name = "ResumeBtn"
         resumeText.zPosition = 5
         resumeButton.addChild(resumeText)
         
-        let quitButton = SKSpriteNode(imageNamed: "redButton")
-        quitButton.size = CGSize(width: 295, height: 76)
+        let quitButton = SKSpriteNode(imageNamed: "restart button")
+        quitButton.size = CGSize(width: 60, height: 60)
         // Name the start node for touch detection:
         quitButton.name = "QuitBtn"
-        quitButton.position = CGPoint(x: 0, y: -150)
+        quitButton.position = CGPoint(x: -45, y: -70)
+        quitButton.zPosition = 5
         
         
         self.addChild(quitButton)
         
-        let quitText = SKLabelNode(fontNamed:
-                                        "AdventPro-Bold")
+        let settingsButton = SKSpriteNode(imageNamed: "settings")
+        settingsButton.size = CGSize(width: 60, height: 60)
+        // Name the start node for touch detection:
+        settingsButton.name = "SettingsBtn"
+        settingsButton.position = CGPoint(x: 45, y: -70)
+        settingsButton.zPosition = 5
         
-        let quitButtonMessage = "Quit"
-        quitText.text = quitButtonMessage
-        quitText.verticalAlignmentMode = .center
-        quitText.position = .zero
-        quitText.fontSize = 40
-        quitText.fontColor = UIColor(rgb: 0x001273)
-        // Name the text node for touch detection:
-        quitText.name = "QuitBtn"
-        quitText.zPosition = 5
-        quitButton.addChild(quitText)
+        
+        self.addChild(settingsButton)
+        
+//        let quitText = SKLabelNode(fontNamed:
+//                                        "AdventPro-Bold")
+//
+//        let quitButtonMessage = "Quit"
+//        quitText.text = quitButtonMessage
+//        quitText.verticalAlignmentMode = .center
+//        quitText.position = .zero
+//        quitText.fontSize = 40
+//        quitText.fontColor = UIColor(rgb: 0x001273)
+//        // Name the text node for touch detection:
+//        quitText.name = "QuitBtn"
+//        quitText.zPosition = 5
+//        quitButton.addChild(quitText)
         
         let message = "Pause"
         
@@ -71,7 +89,7 @@ class PauseScreen: SKSpriteNode {
         label.text = message
         label.fontSize = 60
         label.fontColor = UIColor(rgb: 0xC5DC82)
-        label.position = CGPoint(x: 0, y: 100)
+        label.position = CGPoint(x: 0, y: 70)
         label.zPosition = 5
         addChild(label)
     }
@@ -80,26 +98,26 @@ class PauseScreen: SKSpriteNode {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event:
-                               UIEvent?) {
-        for touch in (touches) {
-            // Find the location of the touch:
-            let location = touch.location(in: self)
-            // Locate the node at this location:
-            let nodeTouched = atPoint(location)
-            if nodeTouched.name == "ResumeBtn" {
-                self.removeFromParent()
-                // Player touched the start text or button node
-                // Switch to an instance of the GameScene:
-//                if let view = self.view {
-//                    if let scene = SKScene(fileNamed: "GameScene") {
-//                        scene.size = view.frame.size
-//                        scene.scaleMode = .aspectFill
-//
-//                        view.presentScene(scene)
-//                    }
-//                }
-            }
-        }
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event:
+//                               UIEvent?) {
+//        for touch in (touches) {
+//            // Find the location of the touch:
+//            let location = touch.location(in: self)
+//            // Locate the node at this location:
+////            let nodeTouched = atPoint(location)
+////            if nodeTouched.name == "ResumeBtn" {
+////                self.removeFromParent()
+////                // Player touched the start text or button node
+////                // Switch to an instance of the GameScene:
+//////                if let view = self.view {
+//////                    if let scene = SKScene(fileNamed: "GameScene") {
+//////                        scene.size = view.frame.size
+//////                        scene.scaleMode = .aspectFill
+//////
+//////                        view.presentScene(scene)
+//////                    }
+//////                }
+////            }
+//        }
+//    }
 }
